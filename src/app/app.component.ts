b42d969c6be9bc17d3944dcb56476ab0161ac193
Shekhar <shekhar.rulz@gmail.com>
2018-09-08 18:17:15 +0530

    Login Websocket Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,45 @@ import { WebsocketRunnerService } from './websocket-runner.service';
 })
 export class AppComponent {
   title = 'projectJanice';
+  serverStatus:string = "off";
+  stompclient:any;
+  loggedInStatus:any;
+  destin:any = '/topic/notification';
+  unsub:any;
+
+  constructor(private sock: WebsocketRunnerService){
+    this.webSockServiceConnect();
+    this.stompclient.debug;
+  }
+
+  webSockServiceConnect(){
+    this.stompclient = this.sock.connect();
+    this.webSockConnect();
+  }
+
+  webSockConnect(){
+    this.stompclient.connect({},frame =>{
+      this.serverStatus = "on";
+      this.webSockSubscriber();
+    },fail=>{
+      this.serverStatus = "off";
+      console.log("Reconnecting WS");
+      window.setTimeout(this.webSockServiceConnect(),2500);
+    }
+  );
+  }
+
+  dataSender(data: any){
+    this.stompclient.send("/app/message",{},data);
+  }
+
+  setLoggedInStatus(value:boolean){
+    this.sock.setloggedInStatus(value);
+  }
+  webSockSubscriber(){
+    this.unsub = this.stompclient.subscribe(this.destin, notifications => {
+    this.loggedInStatus = JSON.parse(notifications.body).loggedInStatus;
+    console.log(this.loggedInStatus);
+    });
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,44 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { Router } from '@angular/router';
-import { WebsocketRunnerService } from '../websocket-runner.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   stompclient:any;
   loggedInStatus:any;
 
-  constructor(private sock: WebsocketRunnerService, private router: Router){
-    this.stompclient = this.sock.connect();
-    this.stompclient.debug;
-    this.stompclient.connect({},frame =>{
-
-      this.stompclient.subscribe('/topic/notification', notifications => {
-
-        this.loggedInStatus = JSON.parse(notifications.body).loggedInStatus;
-        console.log(this.loggedInStatus);
-        if(this.loggedInStatus){
-          this.sock.setloggedInStatus(true);
-          this.router.navigate(['admin']); 
-        }
-        else{
-          window.alert(this.loggedInStatus);
-        }
-      })
-    });
+  constructor(private sock:AppComponent){
+    
   }
 
-  ngOnInit() {
-  }
-  
   loginEvent(event){
+    this.sock.unsub.unsubscribe();
+    this.sock.destin = '/topic/google';
+    this.sock.webSockSubscriber();
     event.preventDefault();
-    const target = event.target
+    const target = event.target;
     const username = target.querySelector('#uname1').value;
     const password = target.querySelector('#pwd1').value;
     console.log(username,password);
@@ -46,6 +28,7 @@ export class LoginComponent implements OnInit {
       'uname' : username,
       'password' : password,
     });
-    this.stompclient.send("/app/message",{},data);
+    this.sock.dataSender(data);
+    
   }
 }
